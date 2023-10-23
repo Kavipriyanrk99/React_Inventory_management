c3c57eb3bcc58c81b5458f09f2015e7010bc17a0
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2023-10-23 14:47:36 +0530

    Clear working router created

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,17 @@ import ProductsBtn from './ProductsBtn';
 import TransactionsBtn from './TransactionsBtn'; 
 import ProductInBtn from './ProductInBtn';
 import ProductOutBtn from './ProductOutBtn';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return(
         <nav className="NavBar">
             <div className='InnerCont'>
-                <DashboardBtn />
-                <ProductsBtn />
-                <TransactionsBtn />
-                <ProductInBtn />
-                <ProductOutBtn />
+                <Link to='/'><DashboardBtn /></Link>
+                <Link to='products'><ProductsBtn /></Link>
+                <Link to='transactions'><TransactionsBtn /></Link>
+                <Link to='productin'><ProductInBtn /></Link>
+                <Link to='productout'><ProductOutBtn /></Link>
             </div>
         </nav>
     );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(
+/* React 18 way of creating... */
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
